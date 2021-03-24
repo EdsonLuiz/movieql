8163055bc9680cdcfff136dadaf456f5aee23e52
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-24 16:47:27 -0300

    feat: add queries, mutations schema and resolvers

---
+++ src/graphql/db.js
@@ -0,0 +1,31 @@
+export let movies = [
+  {id: 1, name: 'Liga da Justiça de Zack Snyder', score: 8.3 },
+  {id: 2, name: 'O Poderoso Chefão', score: 9.3 },
+  {id: 3, name: 'O Lobo de Wall Street', score: 8.2 },
+  {id: 4, name: 'Bastardos Inglórios', score: 8.3 },
+]
+
+export function getMovies() {
+  return movies
+}
+
+export function getById(id) {
+  const returnedMovie = movies.find(movie => movie.id === id)
+  if(!returnedMovie)
+    return {}
+  return returnedMovie
+}
+
+export function deleteMovie(id) {
+  if(!getById(id))
+    return
+
+  movies = movies.filter(m => m.id !== id)
+}
+
+export function create(name, score) {
+  const id = movies.length + 1
+  const newMovie = {id, name, score}
+  movies.push(newMovie)
+  return newMovie
+}
--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,20 @@
+import {getMovies, getById, create} from './db'
+
+const edson = {
+  name: 'Edson Luiz',
+  language: 'pt-br',
+  state: 'RS'
+}
+
 const resolvers = {
   Query: {
-    name: () => 'Edson Luiz'
+    movies: () => getMovies(),
+    movie: (_, {id}) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, {name, score}) => create(name, score)
   }
+
 }
 
 export default resolvers
