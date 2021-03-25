f0337418c5d83e5283ac93ee44ef0be89ead3855
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-25 09:53:07 -0300

    chore: add axios

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,32 +1,5 @@
-export let movies = [
-  {id: 1, name: 'Liga da Justiça de Zack Snyder', score: 8.3 },
-  {id: 2, name: 'O Poderoso Chefão', score: 9.3 },
-  {id: 3, name: 'O Lobo de Wall Street', score: 8.2 },
-  {id: 4, name: 'Bastardos Inglórios', score: 8.3 },
-]
+const API_URL = ' https://yts.mx/api/v2/list_movies.json'
 
 export function getMovies() {
-  return movies
-}
-
-export function getById(id) {
-  const returnedMovie = movies.find(movie => movie.id === id)
-  if(!returnedMovie)
-    return {}
-  return returnedMovie
-}
-
-export function deleteMovie(id) {
-  if(!getById(id))
-    return false
-
-  movies = movies.filter(m => m.id !== id)
-  return true
-}
-
-export function create(name, score) {
-  const id = movies.length + 1
-  const newMovie = {id, name, score}
-  movies.push(newMovie)
-  return newMovie
+  
 }
